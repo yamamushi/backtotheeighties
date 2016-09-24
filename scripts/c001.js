4278de6fb5716a7d56edcfeb9ec5631578ca0250
yamamushi <yamamushi@gmail.com>
2016-09-24 06:20:45 -0500

    c001

--- scripts/c001.js
+++ scripts/c001.js
@@ -1,25 +1,12 @@
 function(context, args)
 {
-	var l = {};
-	var t;
-
 	var u = ["cyan", "purple", "yellow", "red", "green", "orange", "blue", "lime"]
-	c001();
-	tell(JSON.stringify(t));
-	return { ok:true };
-	function c001() {
-
-		t = { c001:"", color_digit:""};
-
-		for (var i = 0; i < u.length; i++) {
-			t.c001 = u[i];
-			t.color_digit = t.c001.length;
-			if (! /LOCK_ERROR.*correct color name\./.test(args.n.call(t).replace("\n", ""))) {
-				break;
-			}
-		}		
-	}
-	function tell(s) {
-		#s.chats.tell({ to:context.caller, msg: s });
-	}
+	for (var i = 0; i < u.length; i++) {
+		var t = { c001:u[i], color_digit:u[i].length};
+		if (! /.*ERROR.*color name\./.test(args.n.call(t).replace("\n", ""))) {
+			#s.chats.tell({ to:context.caller, msg: JSON.stringify(t)});
+			return { ok:true };
+		}
+	}		
+	return { ok:false };
 }
